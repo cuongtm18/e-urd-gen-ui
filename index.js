cbc98d907f31edba31d5d9064d88dd3cfd3df630
cuongvinh <trinhmanh.cuong@etc.vn>
2023-02-01 13:57:27 +0700

    Initial commit

--- index.js
+++ index.js
@@ -3,31 +3,36 @@ const puppeteer = require("puppeteer");
 let url = "http://doc.cs.etc.vn/login";
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false,
-    args: ["--use-gl=egl"],
-    ignoreDefaultArgs: ["--disable-extensions"],
+    headless: true
   });
   const page = await browser.newPage();
   await page.goto(url);
 
-  try {
-    await page.type("#txtAccount", "cuongtm");
-    await page.type("#txtPassword", "Ctm@113995");
-    await page.click(".login100-form-btn");
-    await page.waitForNavigation();
-    await page.goto('http://doc.cs.etc.vn/mhs/lc/Haiquan/MHS_0010.%20Phan%20cong%20xu%20ly%20ho%20so%20xac%20dinh%20truoc%20ma%20so/index.htm')
+  await page.type("#txtAccount", "cuongtm");
+  await page.type("#txtPassword", "Ctm@113995");
+  await page.click(".login100-form-btn");
+  await page.waitForNavigation();
+  await page.goto('http://doc.cs.etc.vn/mhs/lc/Haiquan/MHS_0010.%20Phan%20cong%20xu%20ly%20ho%20so%20xac%20dinh%20truoc%20ma%20so/index.htm')
 
-    const elementHandle = await page.$(`#tocIFrame`);
-    const frame = await elementHandle.contentFrame();
-    const body = await frame.$('body')
-    const tocRoot = await body.$$('ul')
-    tocRoot.forEach(e => {
-      console.log(e)
+  const elementHandle = await page.$(`#tocIFrame`);
+  await page.waitForTimeout(2000)
+  const frame = await elementHandle.contentFrame();
+  console.log(await frame.content())
+  const menu = await frame.waitForSelector('#toc0')
+
+  const data = await menu.evaluate(() => {
+    const eachMenu = document.querySelectorAll('li')
+    let arrData = []
+    eachMenu.forEach(() => {
+      const ul = document.querySelectorAll('ul')
+      arrData.push(ul.innerHTML)
+
     })
-    console.log(tocRoot)
-  } catch (e) {
-    console.log(e);
-  }
+    return {
+      arrData
+    }
+  })
+  console.log('data evaluate', data)
 
   // await browser.close();
 })();
